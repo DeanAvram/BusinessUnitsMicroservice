c2b9c70ba13ad60e2d559945b11a73533bc64396
DeanAvram <dean6198@gmail.com>
2024-06-13 11:31:04 +0300

    Added resources and boundaries

---
+++ src/main/java/boundries/UnitBoundary.java
@@ -0,0 +1,54 @@
+package boundries;
+
+
+public class UnitBoundary {
+
+    private String Id;
+    private String type;
+    private String creationDate;
+    private EmployeeBoundary manager;
+    private UnitBoundary parentUnit;
+
+    public UnitBoundary() {
+    }
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String id) {
+        Id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(String creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public EmployeeBoundary getManager() {
+        return manager;
+    }
+
+    public void setManager(EmployeeBoundary manager) {
+        this.manager = manager;
+    }
+
+    public UnitBoundary getParentUnit() {
+        return parentUnit;
+    }
+
+    public void setParentUnit(UnitBoundary parentUnit) {
+        this.parentUnit = parentUnit;
+    }
+}
